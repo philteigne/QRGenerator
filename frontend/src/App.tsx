5b87452f2b4afc32a1d9348b67d671c8bc8b811d
Phil Teigne <philippe.teigne@gmail.com>
2024-09-21 09:20:57 -0400

    added canvas to display QR code and button to call generateQRCodeWeb

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useState, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import generateQRCodeWeb from './helpers/generateQR'
+import { text } from 'stream/consumers';
+
 function App() {
+
+  const [textInput, setTextInput] = useState('')
+  const [generatorView, setGeneratorView] = useState<Boolean>(false);
+
+  const QRCanvasRef = useRef<HTMLCanvasElement | null>(null); // Reference to the canvas
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +20,18 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <input onChange={e => {
+          setTextInput(e.target.value)
+          setGeneratorView(true)
+          }}></input>
+        
+        <canvas ref={QRCanvasRef}>Hello</canvas>
+
+        <button onClick={() => generateQRCodeWeb(QRCanvasRef, textInput)}>
+          Generate
+        </button>
+
+        
         <a
           className="App-link"
           href="https://reactjs.org"
