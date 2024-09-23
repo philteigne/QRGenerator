95699d1abee450d8c8ac9625e0d9be800c12ab3c
Phil Teigne <philippe.teigne@gmail.com>
2024-09-22 22:36:49 -0400

    added aButtons into QRDisplay

--- frontend/src/components/QRDisplay.tsx
+++ frontend/src/components/QRDisplay.tsx
@@ -2,6 +2,14 @@ import React, { useEffect, useRef } from 'react';
 import { generateQRCodeWeb } from '../helpers/generateQR';
 import { generateQRCodeFile } from '../helpers/generateQR';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShareNodes, faDownload, faPenToSquare, faPlus, faXmark } from '@fortawesome/free-solid-svg-icons'
+
+{/* <FontAwesomeIcon size='3x' icon={faShareNodes} />
+  <FontAwesomeIcon size='3x' icon={faDownload} />
+  <FontAwesomeIcon size='3x' icon={faPenToSquare} />
+  <FontAwesomeIcon size='3x' icon={faPlus} />
+  <FontAwesomeIcon icon={faXmark} /> */}
 
 const QRDisplay = ({ input }: { input: string }) => {
 
@@ -19,9 +27,13 @@ const QRDisplay = ({ input }: { input: string }) => {
       <canvas className='QRCanvas' ref={QRCanvasRef}></canvas>
       <div className='QRInfo'>
         <h3>{input}</h3>
-        <div>
-          <button onClick={() => generateQRCodeFile(input)}>S</button>
-          <button onClick={() => generateQRCodeFile(input)}>D</button>
+        <div className='QRControl'>
+          <a className='aButton inverse' onClick={() => generateQRCodeFile(input)}>
+            <FontAwesomeIcon icon={faShareNodes} />
+          </a>
+          <a className='aButton inverse' onClick={() => generateQRCodeFile(input)}>
+            <FontAwesomeIcon icon={faDownload} />
+          </a>
         </div>
       </div>
     </div>
