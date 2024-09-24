1d002060d1bcf89a491de8ad77e9e4ded8daca12
Phil Teigne <philippe.teigne@gmail.com>
2024-09-24 18:50:24 -0400

    implemented reducer to manage state

---
+++ frontend/src/hooks/stateReducer.tsx
@@ -0,0 +1,53 @@
+import { useReducer } from 'react';
+import { stateObject, actionObject } from '../interfaces/StateInterfaces'
+
+
+const initialState: stateObject = {
+  textInput: '',
+  arrayInput: [''],
+  inputType: 'listInput',
+  appView: 'input',
+  errorMsg: '',
+};
+
+const reducer = (state:stateObject, action:actionObject): stateObject => {
+  switch (action.type) {
+    case "SET_TEXT_INPUT":
+      return {
+        ...state,
+        textInput: action.payload,
+      }
+    case "SET_ARRAY_INPUT":
+      return {
+        ...state,
+        arrayInput: action.payload,
+      }
+    case "SET_INPUT_TYPE":
+      return {
+        ...state,
+        inputType: action.payload,
+      }
+    case "SET_APP_VIEW":
+      return {
+        ...state,
+        appView: action.payload,
+      }
+    case "SET_ERROR_MSG":
+      return {
+        ...state,
+        errorMsg: action.payload,
+      }
+    
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
+const useApplicationData = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return {state, dispatch};
+}
+
+export default useApplicationData;
