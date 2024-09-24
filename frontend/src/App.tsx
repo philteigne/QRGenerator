fdae4e5ec38686b9c0a49e3e678d7ea0213caa27
Phil Teigne <philippe.teigne@gmail.com>
2024-09-24 18:54:45 -0400

    Moved handlers to InputRoute to avoid props

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,64 +11,6 @@ function App() {
 
   const { state, dispatch } = useApplicationData();
 
-  const handleListInputChange = (index: number, newValue: string) => {
-    const updatedItems = [...state.arrayInput];
-
-    updatedItems[index] = newValue;
-    dispatch({type: "SET_ARRAY_INPUT", payload: updatedItems});
-  }
-
-  const handleListInputAdd = () => {
-    const updatedItems = [...state.arrayInput, '']
-
-    dispatch({type: "SET_ARRAY_INPUT", payload: updatedItems})
-  }
-
-  const handleListInputDelete = (index: number) => {
-    const updatedItems = [...state.arrayInput]
-    updatedItems.splice(index, 1)
-    
-    dispatch({type: "SET_ARRAY_INPUT", payload: updatedItems})
-  }
-
-  const handleGeneration = (input: string, inputType: string) => {
-
-    let error = '';
-
-    if (inputType === 'listInput') {
-      // Check for errors
-      if (!state.arrayInput.find(item => item.length > 0)) {
-        error = ('Input should include at least one item')
-      }
-
-    } else if (inputType === 'basicInput') {
-
-      try {
-        dispatch({type: "SET_ARRAY_INPUT", payload: JSON.parse(input)});
-      } catch(e) {
-        error = ('Input is not valid JSON')
-      }
-
-    } else if (inputType === 'textInput') {      
-      const newInput = input.split(';').map(item => item.trim()).filter((item) => item.length > 0)
-
-      if (!newInput.find(item => item.length > 0)) {
-        error = ('Input should include at least one character')
-      } else if (newInput.find(item => item.length > 2331)) {
-        error = ('Input items should each be less than 2332 characters')
-      }
-
-      dispatch({type: "SET_ARRAY_INPUT", payload: newInput});
-    }
-
-    if (error) {
-      dispatch({type: "SET_ERROR_MSG", payload: error});
-      return
-    }
-
-    dispatch({type: "SET_APP_VIEW", payload: 'output'});
-  }
-
   return (
     <div className="App">
       <header>
@@ -79,10 +21,6 @@ function App() {
         <InputRoute
           state={state}
           dispatch={dispatch}
-          handleListInputChange={handleListInputChange}
-          handleListInputAdd={handleListInputAdd}
-          handleListInputDelete={handleListInputDelete}
-          handleGeneration={handleGeneration}
         />
       }
 
