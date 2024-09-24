1847f561421bcd472385edb811181cdfd580a799
Phil Teigne <philippe.teigne@gmail.com>
2024-09-24 16:35:54 -0400

    removed unnecessary comments and imports

--- frontend/src/components/QRDisplay.tsx
+++ frontend/src/components/QRDisplay.tsx
@@ -1,15 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { generateQRCodeWeb } from '../helpers/generateQR';
 import { generateQRCodeFile } from '../helpers/generateQR';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShareNodes, faDownload, faPenToSquare, faPlus, faXmark } from '@fortawesome/free-solid-svg-icons'
-
-{/* <FontAwesomeIcon size='3x' icon={faShareNodes} />
-  <FontAwesomeIcon size='3x' icon={faDownload} />
-  <FontAwesomeIcon size='3x' icon={faPenToSquare} />
-  <FontAwesomeIcon size='3x' icon={faPlus} />
-  <FontAwesomeIcon icon={faXmark} /> */}
+import { faShareNodes, faDownload } from '@fortawesome/free-solid-svg-icons'
 
 const QRDisplay = ({ input }: { input: string }) => {
 
@@ -21,7 +15,7 @@ const QRDisplay = ({ input }: { input: string }) => {
       generateQRCodeWeb(QRCanvasRef, input);
     }
   }, []);
-  
+
   return (
     <div className='QRDisplay'>
       <canvas className='QRCanvas' ref={QRCanvasRef}></canvas>
