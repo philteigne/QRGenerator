c7634001ab182977df11f52349a3c1cf6befca11
Phil Teigne <philippe.teigne@gmail.com>
2024-09-27 10:08:15 -0400

    created QRSettingsObject and added it to stateObject

--- frontend/src/interfaces/StateInterfaces.tsx
+++ frontend/src/interfaces/StateInterfaces.tsx
@@ -1,9 +1,18 @@
+interface QRSettingsObject {
+  errorCorrection:'L' | 'M' | 'Q' | 'H',
+  height:Number,
+  width:Number,
+  margin:Number,
+}
+
 interface stateObject {
   textInput:string,
   arrayInput:string[],
   inputType:'listInput' | 'basicInput' | 'textInput',
   appView:'input' | 'output',
-  errorMsg:string
+  errorMsg:string,
+  QRSettingsModal:Boolean,
+  QRSettings:QRSettingsObject
 }
 
 interface actionObject {
@@ -11,4 +20,4 @@ interface actionObject {
   payload:any,
 }
 
-export type {stateObject, actionObject}
+export type {stateObject, actionObject, QRSettingsObject}
